9405d0db8dbd18d855435d9b2061769740cf60d3
henriq-azvd <106310133+henriq-azvd@users.noreply.github.com>
2022-09-22 17:16:22 -0300

    Exercicio 22-09-2022

---
+++ DiceApp/app/src/main/java/me/gen/diceapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package me.gen.diceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*
+        * findViewById()
+        */
+
+        val buttonD6 = findViewById<Button>(R.id.buttonD6)
+
+        buttonD6.setOnClickListener {
+            // Toast.makeText(this, "Botão  clicado", Toast.LENGTH_LONG).show()
+            rolarDados(6)
+        }
+            val buttonD12 = findViewById<Button>(R.id.buttonD12)
+
+            buttonD12.setOnClickListener {
+                // Toast.makeText(this, "Botão  clicado", Toast.LENGTH_LONG).show()
+                rolarDados(12)
+            }
+                val buttonD20 = findViewById<Button>(R.id.buttonD20)
+
+                buttonD20.setOnClickListener {
+                    // Toast.makeText(this, "Botão  clicado", Toast.LENGTH_LONG).show()
+                    rolarDados(20)
+        }
+    }
+
+    private fun rolarDados(lados: Int) {
+        val rolagem = (1..lados).random()
+
+        val textDado = findViewById<TextView>(R.id.textDado)
+
+        textDado.text = rolagem.toString()
+
+    }
+}
